c4e491123d640b9b7ac8f1db3d82b0be12d27a1e
john <john.n.pham2000@gmail.com>
2022-04-10 23:02:32 -0400

    Began using referenced textbook code

--- ConcurrentLinkedList.java
+++ ConcurrentLinkedList.java
@@ -1,7 +1,51 @@
-public class ConcurrentLinkedList
+public class ConcurrentLinkedList 
 {
+	private class Node
+	{
+		T item;
+		int key;
+		Node next;
+	}
+
+	class Window 
+	{
+		public Node pred, curr;
+		Window(Node myPred, Node myCurr)
+		{
+			pred = myPred; curr = myCurr;
+		}
+	}
+	
+	public Window find(Node head, int key) 
+	{
+		Node pred = null, curr = null, succ = null;
+		boolean[] marked = { false };
+		boolean snip;
+		retry: while (true) 
+		{
+			pred = head;
+			curr = pred.next.getReference();
+			while (true) 
+			{
+				succ = curr.next.get(marked);
+				while (marked[0]) 
+				{
+					snip = pred.next.compareAndSet(curr, succ, false, false);
+					if (!snip)
+						continue retry;
+					curr = succ;
+					succ = curr.next.get(marked);
+				}
+				if (curr.key >= key)
+					return new Window(pred, curr);
+				pred = curr;
+				curr = succ;
+			}
+		}
+	}
+
 	public static void main(String [] args)
 	{
-		
+
 	}
 }
--- Party.java
+++ Party.java
@@ -2,6 +2,10 @@ public class Minotaur
 {
 	public static void main(String[] args)
 	{
-		
+		// Number of threads we are using
+		int servants = 4;
+
+		// Number of presents from guests
+		int presents = 500000;
 	}
 }
